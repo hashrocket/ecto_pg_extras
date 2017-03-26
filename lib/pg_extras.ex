cf6532cee155387b3155181ab95ddf35bfdc2089
jbranchaud <jbranchaud@gmail.com>
2017-03-25 19:39:56 -0500

    Remove the Hello World docs

--- lib/pg_extras.ex
+++ lib/pg_extras.ex
@@ -5,19 +5,6 @@ defmodule PgExtras do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PgExtras.hello
-      :world
-
-  """
-  # def hello do
-  #   :world
-  # end
-
-  @doc """
   Use SQL's coalesce function to return the first argument that is not null.
   """
   defmacro coalesce(left, right) do
